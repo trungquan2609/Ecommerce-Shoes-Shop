ec73e0647ff6847fc9b11f281b09fa18459835b4
trungquan2609 <nguyentrungquan.2609@gmail.com>
2022-11-09 00:34:17 +0700

    fix bugs vercel

--- src/routes/api/mod.js
+++ src/routes/api/mod.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Modcontroller = require('../../app/controllers/api/Modcontroller.js');
+const Modcontroller = require('../../app/controllers/api/ModController.js');
 
 router.get('/deleteMod', Modcontroller.delete);
 
